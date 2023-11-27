df530ea217a05a5b7efb219831f681deaff431f0
youssef_safwat_nassralla <youssefsafwat717@gmail.com>
2023-11-27 18:33:58 +0200

    Create linked_list_cycle.cpp
    My solution to the Linked List Cycle problem on LeetCode.

---
+++ linked_list_cycle.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+   
+public:
+    
+    bool hasCycle(ListNode *head) {
+         if (head == nullptr || head->next == nullptr) {
+            return false;  
+        }
+         ListNode* slow = head;
+         ListNode* fast = head->next;
+       while (fast != slow) {
+           if(fast ==NULL || fast->next == NULL)
+           {
+               return false;
+           }
+           
+        slow = slow->next;
+        fast= fast->next->next;
+       }
+        return true;
+    }
+};
